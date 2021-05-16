bc8661e140a0e5f39ecb68323be0ce04484b0b41
petr.bincik <petr.bincik@onlio.com>
2021-05-16 21:02:47 +0200

    project initialization

---
+++ src/main/kotlin/com/binpe/gooddata/coffeemachines/CoffeemachinesApplication.kt
@@ -0,0 +1,11 @@
+package com.binpe.gooddata.coffeemachines
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoffeemachinesApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoffeemachinesApplication>(*args)
+}
---
+++ src/test/kotlin/com/binpe/gooddata/coffeemachines/CoffeemachinesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.binpe.gooddata.coffeemachines
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoffeemachinesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
